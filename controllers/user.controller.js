18018e5c5dbd6b942459d7cb8c2191ca5bea0837
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2024-04-05 11:19:20 +0530

    feat:mvc structure and server setup

---
+++ controllers/user.controller.js
@@ -0,0 +1,5 @@
+const createUser = async (req, res) => {
+  res.json({ message: "user created" });
+};
+
+export { createUser };
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import connectDB from "./config/db.js";
+import userRouter from "./routes/user.router.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -9,6 +11,12 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 connectDB(process.env.MONGO_URI);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/users", userRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
